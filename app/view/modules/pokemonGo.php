f541f5837ace4d4ab295c5b77f6c5d597265a497
Borja <Borja@mbp-de-borja.wifi.alumnado>
2016-09-13 13:03:23 +0200

    Inicio pagina principal

---
+++ app/view/modules/pokemonGo.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html> 
+
+	<head>
+
+		<title> POKEMON GO MAGAZINE</title>
+
+		
+
+		<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css">
+
+		<link rel="stylesheet" type="text/css" href="../css/pokemonCSS.css">
+
+	</head>
+	
+	<body>
+
+
+		<nav class="uk-navbar">
+
+				<div  class="uk-navbar-nav">
+
+        			<ul class="uk-navbar-nav">
+  					
+  					<li class="logo"> <img src="../img/pokeball.png" id="logo" class="uk-animation-shake"> </li>
+
+  					</ul>
+
+  			</div>
+    	
+
+        	<div  class="uk-navbar-flip">
+
+        			<ul class="uk-navbar-nav ">
+  					
+  					<li class="uk-button-danger"> <a href="#about">LOGIN</a></li>
+
+  					</ul>
+
+  			</div>
+
+		</nav>
+
+		<nav class="uk-navbar">
+
+		<ul class="uk-navbar-nav uk-navbar-center">
+
+    				
+  					<li> <a href="#Noticias" > NOTICIAS </a> </li>
+  					<li> <a href="#news"> TIPS </a> </li>
+  					<li> <a href="#contact"> FAIL </a> </li>
+
+        	</ul>
+
+        </nav>
+
+
+
+
+
+
+<!--
+
+		<div id="slider">
+			
+			<figure>
+
+			<img src="1.jpg" class="Foto">
+			<img src="2.jpg" class="Foto">
+			<img src="3.jpg" class="Foto">
+			<img src="4.jpg" class="Foto">
+			<img src="5.jpg" class="Foto">
+			
+			</figure>
+</div>
+-->
+	</body>
+</html>
