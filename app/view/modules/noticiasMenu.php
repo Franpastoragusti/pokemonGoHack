f2074a02ceae552edbeb92007473059b6ad5198a
Franpastoragusti <franciscopa91@gmail.com>
2016-09-14 23:52:23 +0200

    reconstruido el menu de noticias

---
+++ app/view/modules/noticiasMenu.php
@@ -0,0 +1,192 @@
+<html>
+
+<head>
+
+<title>POKEMON GO</title>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css">
+<link rel="stylesheet" type="text/css" href="../css/styles.css">
+</head>
+
+<body>
+<div class="uk-container-center-personalized uk-flex-row">
+ 	<div class="uk-flex uk-flex-row">
+		<div class="uk-width-2-4 uk-flex uk-flex-row">
+			<div class="uk-width-2-6">
+				<img class="uk-thumbnail uk-thumbnail-medium" src="http://goo.gl/Edvo7v">
+				<form class="uk-form stars" >
+					<div class="uk-form-row">
+					    <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1" value="5">
+				        <label for="rating-input-1-5" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-4" name="rating-input-1">
+				        <label for="rating-input-1-4" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-3" name="rating-input-1"active>
+				        <label for="rating-input-1-3" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-2" name="rating-input-1">
+				        <label for="rating-input-1-2" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-1" name="rating-input-1">
+				        <label for="rating-input-1-1" class="rating-star"></label>
+					</div>
+				</form>
+			</div>
+			<div class="uk-width-2-4 uk-text-left">
+				<h2>¿Por qué lo usamos?</h2>
+				<h4>"Neque porro quisquam est </h4>
+				<p class="uk-text-justify">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum</p>
+			</div>
+		</div>
+		<div class="uk-width-2-4 uk-flex uk-flex-row">
+			<div class="uk-width-2-6">
+				<img class="uk-thumbnail uk-thumbnail-medium" src="http://goo.gl/Edvo7v">
+				<form class="uk-form stars" >
+					<div class="uk-form-row">
+					    <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1" value="5">
+				        <label for="rating-input-1-5" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-4" name="rating-input-1">
+				        <label for="rating-input-1-4" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-3" name="rating-input-1"active>
+				        <label for="rating-input-1-3" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-2" name="rating-input-1">
+				        <label for="rating-input-1-2" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-1" name="rating-input-1">
+				        <label for="rating-input-1-1" class="rating-star"></label>
+					</div>
+				</form>
+			</div>
+			<div class="uk-width-2-4 uk-text-left">
+				<h2 class="">¿Por qué lo usamos?</h2>
+				<h4 class="">"Neque porro quisquam est </h4>
+				<p class="uk-text-justify">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición</p>
+			</div>
+		</div>
+	</div>
+	<div class="uk-flex uk-flex-row">
+		<div class="uk-width-2-4 uk-flex uk-flex-row">
+			<div class="uk-width-2-6">
+				<img class="uk-thumbnail uk-thumbnail-medium" src="http://goo.gl/Edvo7v">
+				<form class="uk-form stars" >
+					<div class="uk-form-row">
+					    <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1" value="5">
+				        <label for="rating-input-1-5" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-4" name="rating-input-1">
+				        <label for="rating-input-1-4" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-3" name="rating-input-1"active>
+				        <label for="rating-input-1-3" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-2" name="rating-input-1">
+				        <label for="rating-input-1-2" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-1" name="rating-input-1">
+				        <label for="rating-input-1-1" class="rating-star"></label>
+					</div>
+				</form>
+			</div>
+			<div class="uk-width-2-4 uk-text-left">
+				<h2 class="">¿Por qué lo usamos?</h2>
+				<h4 class="">"Neque porro quisquam est </h4>
+				<p class="uk-text-justify">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición</p>
+			</div>
+		</div>
+
+		<div class="uk-width-2-4 uk-flex uk-flex-row">
+			<div class="uk-width-2-6">
+				<img class="uk-thumbnail uk-thumbnail-medium" src="http://goo.gl/Edvo7v">
+				<form class="uk-form stars" >
+					<div class="uk-form-row">
+					    <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1" value="5">
+				        <label for="rating-input-1-5" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-4" name="rating-input-1">
+				        <label for="rating-input-1-4" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-3" name="rating-input-1"active>
+				        <label for="rating-input-1-3" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-2" name="rating-input-1">
+				        <label for="rating-input-1-2" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-1" name="rating-input-1">
+				        <label for="rating-input-1-1" class="rating-star"></label>
+					</div>
+				</form>
+			</div>
+			<div class="uk-width-2-4 uk-text-left">
+				<h2 class="">¿Por qué lo usamos?</h2>
+				<h4 class="">"Neque porro quisquam est </h4>
+				<p class="uk-text-justify">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición</p>
+			</div>
+		</div>
+	</div>
+	<div class="uk-flex uk-flex-row">
+		<div class="uk-width-2-4 uk-flex uk-flex-row">
+			<div class="uk-width-2-6">
+				<img class="uk-thumbnail uk-thumbnail-medium" src="http://goo.gl/Edvo7v">
+				<form class="uk-form stars" >
+					<div class="uk-form-row">
+					    <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1" value="5">
+				        <label for="rating-input-1-5" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-4" name="rating-input-1">
+				        <label for="rating-input-1-4" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-3" name="rating-input-1"active>
+				        <label for="rating-input-1-3" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-2" name="rating-input-1">
+				        <label for="rating-input-1-2" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-1" name="rating-input-1">
+				        <label for="rating-input-1-1" class="rating-star"></label>
+					</div>
+				</form>
+			</div>
+			<div class="uk-width-2-4 uk-text-left">
+				<h2>¿Por qué lo usamos?</h2>
+				<h4>"Neque porro quisquam est </h4>
+				<p class="uk-text-justify">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum</p>
+			</div>
+		</div>
+		<div class="uk-width-2-4 uk-flex uk-flex-row">
+			<div class="uk-width-2-6">
+				<img class="uk-thumbnail uk-thumbnail-medium" src="http://goo.gl/Edvo7v">
+				<form class="uk-form stars" >
+					<div class="uk-form-row">
+					    <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1" value="5">
+				        <label for="rating-input-1-5" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-4" name="rating-input-1">
+				        <label for="rating-input-1-4" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-3" name="rating-input-1"active>
+				        <label for="rating-input-1-3" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-2" name="rating-input-1">
+				        <label for="rating-input-1-2" class="rating-star"></label>
+				        <input type="radio" class="rating-input"
+				            id="rating-input-1-1" name="rating-input-1">
+				        <label for="rating-input-1-1" class="rating-star"></label>
+					</div>
+				</form>
+			</div>
+			<div class="uk-width-2-4 uk-text-left">
+				<h2 class="">¿Por qué lo usamos?</h2>
+				<h4 class="">"Neque porro quisquam est </h4>
+				<p class="uk-text-justify">Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición</p>
+			</div>
+		</div>
+	</div>
+	<ul class="uk-pagination">
+		<li class="uk-pagination-previous"><a href=""><i class="uk-icon-angle-double-left"></i></a></li>
+		<li class="uk-pagination-next"><a href=""><i class="uk-icon-angle-double-right"></i></a></li>
+	</ul>
+</div>
+</body>
