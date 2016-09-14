191c27ecfd7797cb1561ce5283bce440b66342a5
Franpastoragusti <franciscopa91@gmail.com>
2016-09-14 06:13:12 +0200

    preparados modales login y registro

--- app/view/modules/page.php
+++ app/view/modules/page.php
@@ -4,8 +4,13 @@
 	<title>PokemonGo Magacine</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css">
+	
+	<link rel="stylesheet" type="text/css" href="app/view/css/pokemonCSS.css">
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js"></script>
 </head>
 <body>
+<header>#HEADER#</header>
 
 </body>
 </html>
