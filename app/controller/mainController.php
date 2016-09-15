31b201bd9f299ce2e178d5fdfa32fc42c4875f9d
Franpastoragusti <franciscopa91@gmail.com>
2016-09-15 12:59:22 +0200

    incluido en php

--- app/controller/mainController.php
+++ app/controller/mainController.php
@@ -1,11 +1,21 @@
 <?php
+
+require_once"pageGenerator.php";
+
+
 class MainController{
 
 
+	public function viewLittleNews(){
+		$pagina=load_template();
+		$header = load_page("app/view/modules/mainHeader.php");
+		$content = load_page("app/view/modules/noticiasMenu.php");
+		$page = replace_header('/\#HEADER\#/ms', $header, $pagina);
+		$page = replace_content('/\#CONTENT\#/ms' ,$content , $page);
+		view_page($page);
+	}
 
 
-
-	
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <?php
-require_once"app/controller/pageGenerator.php";
+require_once"app/controller/mainController.php";
 
+$mvc= new MainController();
 
-$pagina=load_template();
+$mvc->viewLittleNews();
 
-$header = load_page("app/view/modules/mainHeader.php");
-
-$content = load_page("app/view/modules/noticiasMenu.php");
-
-$page = replace_header('/\#HEADER\#/ms', $header, $pagina);
-
-$page = replace_content('/\#CONTENT\#/ms' ,$content , $page);
-
-view_page($page);
 
 
 
