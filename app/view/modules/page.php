1b18ed9e86d0af1d893a7ff9a0a0206e9e818a99
Borja0724 <borja.s0724@gmail.com>
2016-09-16 12:44:13 +0200

    Slider

--- app/view/modules/page.php
+++ app/view/modules/page.php
@@ -5,12 +5,17 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/components/slideshow.min.css">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/uikit.min.css">
 	<link rel="stylesheet" type="text/css" href="app/view/css/styles.css">
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="app/view/css/pokemonCSS.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/components/slideshow.min.js"></script>
 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/components/slideshow.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/css/components/slideshow.min.css">
 </head>
 <body>
 <div id="header">#HEADER#</div>
---
+++ app/view/modules/slider.php
@@ -0,0 +1,16 @@
+<div> 
+
+	<div class="slider">
+
+		<ul class="uk-slideshow" data-uk-slideshow="{autoplay:true}">
+
+    		<li> <img src="1.jpg" alt=""> </li>
+    		<li> <img src="2.jpg" alt=""> </li>
+
+		</ul>
+
+	</div>
+
+</div>
+
+
