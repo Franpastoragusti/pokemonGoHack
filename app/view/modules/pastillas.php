b6a902ad254143585703af1a3904088587d13735
Franpastoragusti <franciscopa91@gmail.com>
2016-09-16 13:45:52 +0200

    asdasda

---
+++ app/view/modules/pastillas.php
@@ -0,0 +1,52 @@
+			<div class="uk-width-2-4 uk-flex uk-flex-row">
+				<div class="uk-width-2-6">
+					<img class="uk-thumbnail uk-thumbnail-medium" src="#DATA#['foto']">
+					<form class="uk-form stars" >
+						<div class="uk-form-row">
+						   
+					        <label for="rating-input-1-5" class="rating-star"></label>
+					      
+					        <label for="rating-input-1-4" class="rating-star"></label>
+					      
+					        <label for="rating-input-1-3" class="rating-star"></label>
+					       
+					        <label for="rating-input-1-2" class="rating-star"></label>
+					        
+					        <label for="rating-input-1-1" class="rating-star"></label>
+						</div>
+					</form>
+				</div>
+				<div class="uk-width-2-4 uk-text-left">
+					<h2>#DATA#["titulo"]</h2>
+					<h4>#DATA#["subtitulo"]</h4>
+					<h5>#DATA#["fecha"]</h5>
+					<h6>#CATEGORIA#[$e]["nombre"]</h6>
+					<p class="uk-text-justify">#DATA#["contenido"]</p>
+				</div>
+			</div>
+			<div class="uk-width-2-4 uk-flex uk-flex-row">
+				<div class="uk-width-2-6">
+					<img class="uk-thumbnail uk-thumbnail-medium" src="#DATA#['foto']">
+					<form class="uk-form stars" >
+						<div class="uk-form-row">
+						   
+					        <label for="rating-input-1-5" class="rating-star"></label>s
+					      
+					        <label for="rating-input-1-4" class="rating-star"></label>
+					      
+					        <label for="rating-input-1-3" class="rating-star"></label>
+					       
+					        <label for="rating-input-1-2" class="rating-star"></label>
+					        
+					        <label for="rating-input-1-1" class="rating-star"></label>
+						</div>
+					</form>
+				</div>
+				<div class="uk-width-2-4 uk-text-left">
+					<h2>#DATA#["titulo"]</h2>
+					<h4>#DATA#["subtitulo"]</h4>
+					<h5>#DATA#["fecha"]</h5>
+					<h6>#CATEGORIA#[$e]["nombre"]</h6>
+					<p class="uk-text-justify">#DATA#["contenido"]</p>
+				</div>
+			</div>';
