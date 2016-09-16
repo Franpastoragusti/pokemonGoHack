991234399b35abee60c634186c7f6269d3a19a64
arturet4 <arturet_vcf@hotmail.com>
2016-09-16 10:15:21 +0200

    creacion funciones clase news

--- app/model/class.news.php
+++ app/model/class.news.php
@@ -1,8 +1,27 @@
 <?php
-require_once"inter_news.php"
-class news implements Inter_news
+//require_once"inter_news.php";
+require_once "class.bbdd.php";
+class news
 {
 	
-	aadqwdwada
+	public function getNewsByCategory($category);
+	public function getNews($id){
+
+		$this->conectar();		
+		$query = $this->consulta("SELECT * from noticias n where n.id_noticias=$id");
+ 	    $this->disconnect();					
+		if($this->numero_de_filas($query) > 0) 
+		{		
+				while ( $tsArray = $this->fetch_assoc($query) ) 
+					$data[] = $tsArray;			
+		
+				return $data;
+		}else
+		{	
+			return '';
+		}	
+
+	}
+	public function getAllNews();
 }
 ?>
